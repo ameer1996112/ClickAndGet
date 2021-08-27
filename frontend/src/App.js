d1f6fe14752605fb0bb9bc1fb8719e728eab4387
Ameer <ameer_1996112@hotmail.com>
2021-08-27 23:36:38 +0300

    Home screen products listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Header />
       <main>
         <h1>Welcome To ClickAndGet</h1>
+        <HomeScreen />
       </main>
 
       <Footer />
