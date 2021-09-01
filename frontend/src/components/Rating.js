ebea72d7265878866c50be2f4f7392fa4638872b
Ameer <ameer_1996112@hotmail.com>
2021-09-01 20:45:07 +0300

    Fetching products from the backend

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Rating = ({ value, text, color = '#f8e825' }) => {
+const Rating = ({ value, text, color }) => {
   return (
     <div className='rating'>
-      <span style={{ color }}>
+      <span>
         <i
+          style={{ color }}
           className={
             value >= 1
               ? 'fas fa-star'
@@ -15,45 +16,49 @@ const Rating = ({ value, text, color = '#f8e825' }) => {
           }
         ></i>
       </span>
-      <span style={{ color }}>
+      <span>
         <i
+          style={{ color }}
           className={
-            value >= 1
+            value >= 2
               ? 'fas fa-star'
-              : value >= 0.5
+              : value >= 1.5
               ? 'fas fa-star-half-alt'
               : 'far fa-star'
           }
         ></i>
       </span>
-      <span style={{ color }}>
+      <span>
         <i
+          style={{ color }}
           className={
-            value >= 1
+            value >= 3
               ? 'fas fa-star'
-              : value >= 0.5
+              : value >= 2.5
               ? 'fas fa-star-half-alt'
               : 'far fa-star'
           }
         ></i>
       </span>
-      <span style={{ color }}>
+      <span>
         <i
+          style={{ color }}
           className={
-            value >= 1
+            value >= 4
               ? 'fas fa-star'
-              : value >= 0.5
+              : value >= 3.5
               ? 'fas fa-star-half-alt'
               : 'far fa-star'
           }
         ></i>
       </span>
-      <span style={{ color }}>
+      <span>
         <i
+          style={{ color }}
           className={
-            value >= 1
+            value >= 5
               ? 'fas fa-star'
-              : value >= 0.5
+              : value >= 4.5
               ? 'fas fa-star-half-alt'
               : 'far fa-star'
           }
@@ -62,6 +67,10 @@ const Rating = ({ value, text, color = '#f8e825' }) => {
       <span>{text && text}</span>
     </div>
   );
+};
+
+Rating.defaultProps = {
+  color: '#f8e825',
 };
 
 Rating.propTypes = {
