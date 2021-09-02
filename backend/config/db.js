1349c2abf8e3fb11af59e60077d424f742a185f9
Ameer <ameer_1996112@hotmail.com>
2021-09-02 20:20:41 +0300

    Model data

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,16 @@
 import mongoose from 'mongoose';
-
+import colors from 'colors';
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
-    console.log(`MongoDB Connected:${conn.connection.host}`);
+    console.log(
+      `MongoDB Connected:${conn.connection.host}`.cyan.underline.bold
+    );
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(`Error: ${error.message}`.red.underline);
     process.exit(1);
   }
 };
