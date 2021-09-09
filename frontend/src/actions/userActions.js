535d4ecda34f552ea7dcf766518324d5e738c9b2
Ameer <ameer_1996112@hotmail.com>
2021-09-09 22:34:31 +0300

    Show user in navbar & logout

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -31,3 +31,7 @@ export const login = (email, password) => async (dispatch) => {
     });
   }
 };
+export const logout = () => (dispatch) => {
+  localStorage.removeItem('userInfo');
+  dispatch({ type: USER_LOGOUT });
+};
