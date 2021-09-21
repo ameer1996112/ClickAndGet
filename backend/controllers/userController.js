55cc2414a4f95aed2d6063563871c926bd8572c1
Ameer <ameer_1996112@hotmail.com>
2021-09-22 00:01:53 +0300

    User profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -49,6 +49,9 @@ const registerUser = asyncHandler(async (req, res) => {
     }
   }
 });
+//@desc GET user profile
+// @route GET /api/users/profile
+//@access Private
 const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
 
@@ -64,5 +67,30 @@ const getUserProfile = asyncHandler(async (req, res) => {
     throw new Error('User not found');
   }
 });
+//@desc UPDATE user profile
+// @route PUT /api/users/profile
+//@access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
 
-export { authUser, registerUser, getUserProfile };
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updatedUser = await user.save();
+    res.json({
+      _id: updatedUser.id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+export { authUser, registerUser, getUserProfile, updateUserProfile };
