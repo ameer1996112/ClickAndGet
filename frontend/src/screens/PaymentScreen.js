74a0a7aa472fa4b9d8de59630d2a57551f56bd7e
Ameer <ameer_1996112@hotmail.com>
2021-09-26 09:09:26 +0300

    Shipping , payment screen & checkout steps

---
+++ frontend/src/screens/PaymentScreen.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+
+import { Form, Button, Col } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import FormContainer from '../components/FormContainer';
+import { savePaymentMethod } from '../actions/cartActions';
+import CheckoutSteps from '../components/CheckoutSteps';
+const PaymentScreen = ({ history }) => {
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+
+  if (!shippingAddress) {
+    history.push('/shipping');
+  }
+  const [paymentMethod, setPaymentMethod] = useState('PayPal');
+  const dispatch = useDispatch();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(savePaymentMethod(paymentMethod));
+  };
+  return (
+    <FormContainer>
+      <CheckoutSteps step1 step2 step3 />
+      <h1>Payment Method</h1>
+      <Form onSubmit={submitHandler}></Form>
+      <Form.Group>
+        <Form.Label as='legend'>Select Method</Form.Label>
+        <Col></Col>
+      </Form.Group>
+    </FormContainer>
+  );
+};
+
+export default PaymentScreen;
