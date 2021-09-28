65facc8fdd29780fc7b14d3f0b09884ddf7d1c55
Ameer <ameer_1996112@hotmail.com>
2021-09-28 19:24:22 +0300

    GetOrderByID, order reducer & action

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,33 +1,36 @@
-import axios from 'axios'
+import axios from 'axios';
+import { ORDER_DETAILS_FAIL } from '../constants/cartConstants';
 import {
   ORDER_CREATE_REQUEST,
   ORDER_CREATE_SUCCESS,
   ORDER_CREATE_FAIL,
-} from '../constants/orderConstants'
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_SUCCESS,
+} from '../constants/orderConstants';
 
 export const createOrder = (order) => async (dispatch, getState) => {
   try {
     dispatch({
       type: ORDER_CREATE_REQUEST,
-    })
+    });
 
     const {
       userLogin: { userInfo },
-    } = getState()
+    } = getState();
 
     const config = {
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${userInfo.token}`,
       },
-    }
+    };
 
-    const { data } = await axios.post(`/api/orders`, order, config)
+    const { data } = await axios.post(`/api/orders`, order, config);
 
     dispatch({
       type: ORDER_CREATE_SUCCESS,
       payload: data,
-    })
+    });
   } catch (error) {
     dispatch({
       type: ORDER_CREATE_FAIL,
@@ -35,6 +38,39 @@ export const createOrder = (order) => async (dispatch, getState) => {
         error.response && error.response.data.message
           ? error.response.data.message
           : error.message,
-    })
+    });
   }
-}
+};
+
+export const getOrderDetails = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: ORDER_DETAILS_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.post(`/api/orders/${id}`, config);
+
+    dispatch({
+      type: ORDER_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ORDER_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
